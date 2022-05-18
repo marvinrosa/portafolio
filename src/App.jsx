2e4301cfffe8111f56e5c49097b8268d04e209ba
Marvin Rosa <marvin.fortin@outlook.com>
2022-05-17 19:01:09 -0600

    agregando el logo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1> <span> <img src={logo} width={50} height={50}/> </span>marvinrosa <span>.dev</span></h1>
+      
+    </div>
+  )
+}
+
+export default App
